f4530511411b781c20f51f088b712d19a7512e67
jollypyun <lucas.stave@gmail.com>
2023-04-24 17:20:31 +0900

    - useState 기본

--- prac/src/components/Expense/ExpenseItem.js
+++ prac/src/components/Expense/ExpenseItem.js
@@ -1,8 +1,13 @@
+import { useState } from 'react'
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.tilte);
+    const clickHandler = () => {
+        console.log('Clicked!!');
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title!</button>
         </Card>
     )
 }
